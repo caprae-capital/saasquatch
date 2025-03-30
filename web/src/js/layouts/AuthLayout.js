2e64c3fc8f07a5ee3f486b354bbc5661800cb6d4
Hyon Lee <git@hyonlee.net>
2025-03-30 00:23:13 -0400

    Fixed redirect loop bug

--- web/src/js/layouts/AuthLayout.js
+++ web/src/js/layouts/AuthLayout.js
@@ -1,42 +1,44 @@
-import React, {useEffect} from "react";
-import {Outlet, useNavigate} from "react-router-dom";
+import React, { useEffect } from "react";
+import { Outlet, useNavigate, useLocation } from "react-router-dom";
 import logo from '../../assets/logo.svg';
 import '../../css/AuthLayout.css';
 import axios from "axios";
 
 const AuthLayout = () => {
     const navigate = useNavigate();
+    const location = useLocation();
 
     useEffect(() => {
         const checkLoggedIn = async () => {
             try {
                 const response = await axios.get('/api/get_current_user');
-
                 if (response.data.subscription_status === 'active') {
                     navigate('/');
                 } else if (response.data.id) {
                     navigate('/plans');
-                } else {
+                } else if (location.pathname !== '/login' && location.pathname !== '/signup') {
                     navigate('/login');
                 }
             } catch (error) {
-                navigate('/login');
+                if (location.pathname !== '/login' && location.pathname !== '/signup') {
+                    navigate('/login');
+                }
             }
         };
 
         checkLoggedIn();
-    }, [navigate]);
+    }, [navigate, location.pathname]);
 
     return (
         <div className="auth-container">
             <div className="auth-logo">
-                <img src={logo || ""} alt="Saasquatch"/>
+                <img src={logo || ""} alt="Saasquatch" />
             </div>
             <div className="auth-modal">
-                <Outlet/>
+                <Outlet />
             </div>
         </div>
-    )
+    );
 };
 
 export default AuthLayout;
