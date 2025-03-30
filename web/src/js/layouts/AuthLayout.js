2192878f1261aea9099afa291b88c9ced3943ec6
Hyon Lee <git@hyonlee.net>
2025-03-30 00:17:15 -0400

    Added check for logged in

--- web/src/js/layouts/AuthLayout.js
+++ web/src/js/layouts/AuthLayout.js
@@ -1,9 +1,32 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, {useEffect} from "react";
+import {Outlet, useNavigate} from "react-router-dom";
 import logo from '../../assets/logo.svg';
 import '../../css/AuthLayout.css';
+import axios from "axios";
 
 const AuthLayout = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const checkLoggedIn = async () => {
+            try {
+                const response = await axios.get('/api/get_current_user');
+
+                if (response.data.subscription_status === 'active') {
+                    navigate('/');
+                } else if (response.data.id) {
+                    navigate('/plans');
+                } else {
+                    navigate('/login');
+                }
+            } catch (error) {
+                navigate('/login');
+            }
+        };
+
+        checkLoggedIn();
+    }, [navigate]);
+
     return (
         <div className="auth-container">
             <div className="auth-logo">
--- web/src/js/layouts/DashboardLayout.js
+++ web/src/js/layouts/DashboardLayout.js
@@ -1,9 +1,28 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useNavigate, Outlet } from "react-router-dom";
+import axios from "axios";
 import Header from '../components/Header';
 import '../../css/DashboardLayout.css';
 
 const DashboardLayout = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const checkLoggedIn = async () => {
+            try {
+                const response = await axios.get('/api/get_current_user');
+
+                if (response.status !== 200 || !response.data.id) {
+                    navigate('/login');
+                }
+            } catch (error) {
+                navigate('/login');
+            }
+        };
+
+        checkLoggedIn();
+    }, [navigate]);
+
     return (
         <div className="dashboard-layout">
             <Header />
